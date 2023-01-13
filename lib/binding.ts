d534bd767076930766552ef45501b4c14aa5db0e
Camden Narzt <c.narzt@me.com>
2023-01-13 15:57:52 -0700

    more of library wrapped

--- lib/binding.ts
+++ lib/binding.ts
@@ -1,4 +1,11 @@
-const addon = require('../build/Release/getargv_native');
+interface CBinding {
+
+    get_argv_of_pid(pid: number, nuls: boolean, skip: number): string
+
+    get_argv_and_argc_of_pid(pid: number): Array<string>
+
+}
+
+const addon: CBinding = require('../build/Release/getargv_native');
 
 export = addon
-
