98b6bfa7a483b8ecb2228aa20339b2162d5eab4b
Camden Narzt <c.narzt@me.com>
2023-01-14 15:09:55 -0700

    fix arg handling in cli tool

--- lib/cli.ts
+++ lib/cli.ts
@@ -1,19 +1,32 @@
 const { get_argv_of_pid } = require('./binding');
 
-const pid = Number.parseInt(process.argv.pop()!);
-if (!Number.isInteger(pid)) throw "pid is not a number";
-
-const nuls_index = process.argv.indexOf("-0");
-const keep_nuls = nuls_index != -1;
-if (keep_nuls) process.argv.splice(nuls_index, 1);
-
-const skip_index = process.argv.indexOf("-s");
-let skip = 0;
-if (skip_index > -1) {
-    skip = Number.parseInt(process.argv[skip_index + 1]);
-    if (!Number.isInteger(skip)) throw "skip is not a number";
+function exit(msg: string) {
+    console.error(msg);
+    process.exitCode = -1;
 }
 
-const args = get_argv_of_pid(pid, !keep_nuls, skip);
+// [node, script] are first two args
+if (process.argv.length < 3) {
+    exit("a pid argument is required");
+} else {
+    const pid = Number.parseInt(process.argv.pop()!);
+    if (!Number.isInteger(pid)) {
+        exit("pid is not a number");
+    } else {
+        const nuls_index = process.argv.indexOf("-0");
+        const keep_nuls = nuls_index != -1;
+        if (keep_nuls) process.argv.splice(nuls_index, 1);
 
-process.stdout.write(args);
+        const skip_index = process.argv.indexOf("-s");
+        let skip = 0;
+        if (skip_index > -1) {
+            skip = Number.parseInt(process.argv[skip_index + 1]);
+            if (!Number.isInteger(skip)) exit("argument to skip flag (-s) is not a number");
+        }
+
+        if (process.exitCode == undefined) {
+            const args = get_argv_of_pid(pid, !keep_nuls, skip);
+            process.stdout.write(args);
+        }
+    }
+}
